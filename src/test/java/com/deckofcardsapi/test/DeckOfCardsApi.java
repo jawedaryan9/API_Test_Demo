4c968be08d401ccb4818454ab64d84a6f78d4f69
jawedaryan9 <jawedaryan9@gmail.com>
2021-01-25 14:09:02 -0500

    adding JUnit Class

---
+++ src/test/java/com/deckofcardsapi/test/DeckOfCardsApi.java
@@ -0,0 +1,11 @@
+package com.deckofcardsapi.test;
+
+import org.junit.Test;
+
+public class DeckOfCardsApi {
+    @Test
+    public void getCardDeck()
+    {
+        System.out.println("HelloWorld");
+    }
+}
